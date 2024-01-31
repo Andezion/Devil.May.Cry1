3f5f2c588b4774529b5294e46512e103555a49d5
Andezion <115638748+Andezion@users.noreply.github.com>
2024-01-31 16:04:55 +0200

    Create vector_utils.h
    Декларация read_vector и display_vector, так же тут надо декларировать другие функции для сортировки.

---
+++ Vol.9/vector_utils.h
@@ -0,0 +1,7 @@
+#ifndef DANTE2023_VECTOR_UTILS_H
+#define DANTE2023_VECTOR_UTILS_H
+
+int read_vector(int *tab, int size, int stop_value);
+void display_vector(const int* tab, int size);
+
+#endif //DANTE2023_VECTOR_UTILS_H
