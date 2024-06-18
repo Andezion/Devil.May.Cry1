29f6a2f025f2b6d0880cb1b2bdada881209172dd
Andezion <115638748+Andezion@users.noreply.github.com>
2024-06-18 15:08:06 +0300

    Update main.c
    Dana jest liczba PI:
    
    float fPI = 3.14159265359;
    double dPI = 3.14159265359;
    
    Wartości z zadania 1.6 Wyświetl liczbę I wyświetl (każdą w osobnej linii) z dokładnością do trzech cyfr po przecinku.

--- Temat 1/Zadanie 8/main.c
+++ Temat 1/Zadanie 8/main.c
@@ -2,10 +2,16 @@
 
 int main()
 {
-    float fPI = 3.14159265359f;
-    double dPI = 3.14159265359;
+    double num1 = 6.0;
+    int num2 = 2;
+    int z = (int)num1 * num2;
+    int c = (int)num1 + num2;
+    int v = (int)num1 - num2;
+    printf("%i %i\n", (int)num1, num2);
 
-    printf("%f\n%f",fPI,dPI);
-
+    printf("%i\n",z);
+    printf("%.2f\n", num1 / num2);
+    printf("%i\n",c);
+    printf("%i",v);
     return 0;
 }
